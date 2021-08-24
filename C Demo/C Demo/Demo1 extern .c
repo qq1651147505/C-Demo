dd69c792e52afdde2b1c470b150e86e02af9c509
qq1651147505 <1651147505@qq.com>
2021-08-24 16:37:41 +0800

    初始C

---
+++ C Demo/C Demo/Demo1 extern .c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int global=100;
+int num=100;
+static int num1=100;
+
+
+int Add(int x,int y)
+{
+
+
+	return x+y;
+
+
+}
+static int Add1(int x,int y)
+{
+
+
+	return x+y;
+
+
+}
